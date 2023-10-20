6974cbb22da1dcee8546656d99b2548ba223f121
Mariele <102813740+ladyMariele@users.noreply.github.com>
2023-10-20 23:24:26 +0800

    add regex challenges solved

---
+++ regex/extract-matches.js
@@ -0,0 +1,3 @@
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Changed this line
+let result = extractStr.match(codingRegex); // Changed this line
---
+++ regex/find-more-than-the-first-match.js
@@ -0,0 +1,3 @@
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/ig; // Changed this line
+let result = twinkleStar.match(starRegex); // Changed this line
