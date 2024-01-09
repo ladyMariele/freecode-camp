0df889c301cbf46126b40b2ad438999fdece1981
Mariele <102813740+ladyMariele@users.noreply.github.com>
2024-01-09 21:25:50 +0800

    Update palindrome-checker.js
    add comments

--- JavaScript Algorithms and Data Structures Projects/palindrome-checker.js
+++ JavaScript Algorithms and Data Structures Projects/palindrome-checker.js
@@ -1,13 +1,13 @@
 function palindrome(str) {
     
     let backwardSpelling = "";
-    
+    // replace non-alphanumeric chars with empty string, and then convert all the remaining chars to lowercase
     str = str.replace(/[^a-zA-Z0-9]/gi, "").toLowerCase()
 
     for (let i = str.length - 1; i >= 0; i--) {
         backwardSpelling += (str[i]) 
     }
-        
+    // use ternary operator to compare the two strings to return a boolean value
     return backwardSpelling === str ? true : false 
 
 }
